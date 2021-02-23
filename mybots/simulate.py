4cf680c89dfa36c12b9ce9ccbdec3cb425e0da0a
ncorwin23 <nicholas.corwin@uvm.edu>
2021-02-22 19:37:35 -0500

    Data subdirectory and simulate commit

--- mybots/simulate.py
+++ mybots/simulate.py
@@ -1,6 +1,10 @@
 import pybullet as p
+import pyrosim.pyrosim as pyrosim
 import pybullet_data
 import time
+import numpy as np
+
+FRAMES = 2400 #iterations of loop
 
 physicsClient = p.connect(p.GUI)
 
@@ -9,11 +13,14 @@ p.setGravity(0,0,-9.8)
 
 planeId = p.loadURDF("plane.urdf")
 bodyID = p.loadURDF("body.urdf")
-
 p.loadSDF("world.sdf")
+pyrosim.Prepare_To_Simulate("body.urdf")
 
-for i in range(1000):
+backLegSensorValues = np.zeros(FRAMES)
+for i in range(FRAMES):
 	p.stepSimulation()
+	backLegSensorValues[i] = pyrosim.Get_Touch_Sensor_Value_For_Link("Back_Leg")
 	time.sleep(1/60)
 
+print(backLegSensorValues)
 p.disconnect()
